e6d73f3fae80e747e0538a0a455359fb47517ac4
nmarati <narayanan.marati@gmail.com>
2021-05-09 11:55:34 -0400

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,62 @@
+
+/**
+ * Use this file to define custom functions and blocks.
+ * Read more at https://makecode.microbit.org/blocks/custom
+ */
+
+enum MyEnum {
+    //% block="one"
+    One,
+    //% block="two"
+    Two
+}
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#f4a261 icon="\uf2de"
+namespace kodely {
+    /**
+     * Returns light level from light sensor 
+     * @param Pin number to which light sensor is connected to
+     */
+    //% help=Returns current light level
+    //% blockId="LightLevel" block="light level | %Pin"
+    export function lightlevel(Pin: AnalogPin): number {
+        let lightLevel=0;
+        lightLevel = pins.map(
+            pins.analogReadPin(Pin),
+            0,
+            1023,
+            0,
+            100
+        );
+        return Math.round(lightLevel);
+    }
+
+   /**
+    * Turn LED on/off connected to pin
+    */
+    //% blockId=turn_LED block="Turn LED $ledonff at %pin"
+    //% ledonff.shadow="toggleOnOff"
+    export function turnLED(pin: AnalogPin, ledonff: boolean): void {
+        if (ledonff) {
+            pins.analogSetPeriod(pin, 100)
+            pins.analogWritePin(pin, 100) // brightness
+        }
+        else {
+            pins.analogWritePin(pin, 0)
+        }
+    }
+
+    /**
+    * Detect Motion
+    * @param Pin number to which motion sensor is connected to
+    **/
+    //% blockId="PIRSensor" block="Motion detected on %pin"
+    export function PIR(pin: DigitalPin): boolean {
+        return (pins.digitalReadPin(pin) == 1) 
+    }
+
+
+}
---
+++ main.ts
@@ -0,0 +1,10 @@
+function doSomething (num: number) {
+    return pins.map(
+    kodely.lightlevel(AnalogPin.P0),
+    0,
+    1023,
+    0,
+    4
+    )
+    led.enable(false)
+}
